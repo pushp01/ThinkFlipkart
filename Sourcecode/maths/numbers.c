7c221c3ad67b5618217f15714be8de13ba6573eb
pushp01 <pushpendra01251999@gmail.com>
2020-04-30 16:02:55 +0530

    removed a bug from numbers.c 	bug:division by zero 		put a condition on y not to be zero

--- Sourcecode/maths/numbers.c
+++ Sourcecode/maths/numbers.c
@@ -7,5 +7,10 @@ int diff(int x,int y){
 //added in testing branch	
 int mul(int x,int y){
 	return x*y;}
+int div(int x,int y){
+	if(y!=0)
+	return x/y;
+	else
+	printf("divide by 0");}
 int main{
 printf(pow(2,3);}
